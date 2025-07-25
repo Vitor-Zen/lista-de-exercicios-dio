a0d478418cd3960d1893ba0c79bcd0991b819617
Zen - Sony Vaio <vitorzen19@gmail.com>
2025-07-25 01:56:31 -0300

    feat: Implementação sétimo exercício

---
+++ src/br/com/vitorzen/listadeexerciciosdio/test/Main.java
@@ -0,0 +1,23 @@
+package br.com.vitorzen.listadeexerciciosdio.test;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner value = new Scanner(System.in);
+
+        System.out.println("Digite true ou falso para a comparação");
+        boolean firstValue = value.nextBoolean();
+
+        System.out.println("Digite true ou falso para a comparação");
+        boolean secondValue = value.nextBoolean();
+
+        if (firstValue && secondValue){
+            System.out.println("Ambos os valores são verdadeiros");
+        } else if (!firstValue && !secondValue) {
+            System.out.println("Ambos os valores são falsos");
+        } else {
+            System.out.println("Os valores são diferentes");
+        }
+    }
+}
