4b70dddfae38cc11a9721a4a769475f4efdf088c
Zen - Sony Vaio <vitorzen19@gmail.com>
2025-08-06 01:36:29 -0300

    fix: ajuste na regra do nono exercício

--- src/br/com/vitorzen/listadeexerciciosdio/nonoexercicio/service/CalculaImcService.java
+++ src/br/com/vitorzen/listadeexerciciosdio/nonoexercicio/service/CalculaImcService.java
@@ -11,6 +11,19 @@ public class CalculaImcService extends ReceiveImc implements ImcCalculator {
     @Override
     public void calculaImc() {
         double resultIMC = (peso / (altura * altura));
-        System.out.printf("IMC: %.2f\n", resultIMC);
+
+        if (resultIMC < 18.5) {
+            System.out.printf("IMC abaixo do peso %.2f\n", resultIMC);
+        } else if (resultIMC >= 18.5 && resultIMC <= 24.9) {
+            System.out.printf("IMC peso ideal: %.2f\n", resultIMC);
+        } else if (resultIMC >= 25 && resultIMC <= 29.9) {
+            System.out.printf("IMC levemente acima do peso %.2f\n", resultIMC);
+        } else if (resultIMC >= 30 && resultIMC <= 34.9) {
+            System.out.printf("IMC obesidade grau 1 %.2f\n", resultIMC);
+        } else if (resultIMC >= 35 && resultIMC <= 39.9) {
+            System.out.printf("IMC obesidade grau 2 %.2f\n", resultIMC);
+        } else {
+            System.out.printf("IMC obesidade grau 3 %.2f\n", resultIMC);
+        }
     }
 }
